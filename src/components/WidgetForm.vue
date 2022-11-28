09c8f3d2b0043735965186489285e6c7144a89db
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2022-11-28 16:43:15 -0300

    Workshop ready

--- src/components/WidgetForm.vue
+++ src/components/WidgetForm.vue
@@ -46,59 +46,27 @@ async function handleTakeScreenshot(): Promise<void> {
   image.value = base64Image;
 }
 
-async function createIssueOnGitHub(body: GitHubCreateIssueInterface) {
-  try {
-    const res = await fetch(
-      `https://${config.PROJECT_ID}.api.deskree.com/api/v1/integrations/github/repos/${config.GITHUB_USERNAME}/feedback-tool/issues`,
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      }
-    );
-
-    if (!res.ok) {
-      throw new Error('Error creating issue on GitHub');
-    }
-  } catch (e: any) {
-    console.error(e);
-    showError.value = true;
-    loading.value = false;
-    error.value = e;
-  }
-}
-
 async function createFeedback(body: SendFeedbackBodyInterface) {
   try {
-    const res = await fetch(
-      `https://${config.PROJECT_ID}.api.deskree.com/api/v1/rest/collections/feedbacks`,
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      }
-    );
-
-    if (!res.ok) {
-      throw new Error('Error creating feedback');
-    }
-
-    const { data } = await res.json();
-
-    if (data !== undefined) {
-      emit('feedback_uid', data.uid);
-    } else {
-      throw new Error("Couldn't create feedback");
-    }
+    // TODO: Create Feedback
   } catch (e: any) {
     console.error(e);
     showError.value = true;
-    loading.value = false;
     error.value = e;
   } finally {
     if (!showError.value) {
       image.value = undefined;
     }
+  }
+}
+
+async function createIssueOnGitHub(body: GitHubCreateIssueInterface) {
+  try {
+    // TODO: Create Issue on GitHub
+  } catch (e: any) {
+    console.error(e);
+    showError.value = true;
+    error.value = e;
   }
 }
 
@@ -131,13 +99,14 @@ async function sendFeedback() {
       if (showError.value) return;
     }
     message.value = '';
-    loading.value = false;
     emit('success');
   } catch (e: any) {
     console.error(e);
     showError.value = true;
-    loading.value = false;
     error.value = e;
+  }
+  finally {
+    loading.value = false;
   }
 }
 
@@ -155,26 +124,14 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div
-    class="bg-[#1E1E20] rounded-2xl mb-4 shadow-lg w-[calc(100vw-2rem)] min-h-[390px] md:w-auto sm:w-auto"
-  >
+  <div class="bg-[#1E1E20] rounded-2xl mb-4 shadow-lg w-[calc(100vw-2rem)] min-h-[390px] md:w-auto sm:w-auto">
     <header class="flex flex-col item-center rounded-t-2xl p-3.5">
-      <img
-        src="/icons/left-arrow.svg"
-        alt="Back arrow"
-        class="w-5 h-4 cursor-pointer"
-        @click="$emit('back')"
-      />
+      <img src="/icons/left-arrow.svg" alt="Back arrow" class="w-5 h-4 cursor-pointer" @click="$emit('back')" />
       <CloseButton />
       <div class="flex items-center mt-4 gap-2">
         <span
-          class="flex items-center justify-center bg-gradient-to-r from-deskree-500 to-deskree-600 rounded-full w-8 h-8"
-        >
-          <img
-            :src="props.feedback.icon"
-            :alt="props.feedback.title"
-            class="w-4 h-5"
-          />
+          class="flex items-center justify-center bg-gradient-to-r from-deskree-500 to-deskree-600 rounded-full w-8 h-8">
+          <img :src="props.feedback.icon" :alt="props.feedback.title" class="w-4 h-5" />
         </span>
         <h3 class="font-medium">{{ props.feedback.title }}</h3>
       </div>
@@ -184,53 +141,32 @@ watchEffect(() => {
     </header>
     <div class="flex px-4 gap-2 w-full flex-col">
       <form @submit.prevent="sendFeedback" class="w-full">
-        <textarea
-          v-model="message"
+        <textarea v-model="message"
           class="min-w-[227px] w-full min-h-[150px] text-sm placeholder-white/[0.8] text-white bg-white/[0.08] rounded-lg border-0 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-          placeholder="Share with details your feedback..."
-        />
+          placeholder="Share with details your feedback..." />
         <div class="flex flex-col gap-4 mt-2">
-          <span
-            v-if="image === undefined"
+          <span v-if="image === undefined"
             class="text-xs text-deskree-300 hover:text-white cursor-pointer transition-colors"
-            @click="handleTakeScreenshot"
-            >Take a screenshot</span
-          >
-          <button
-            v-else
-            type="button"
-            @click="image = undefined"
+            @click="handleTakeScreenshot">Take a screenshot</span>
+          <button v-else type="button" @click="image = undefined"
             class="p-1 w-8 h-8 rounded-md border-2 border-deskree-300 flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
             :style="{ backgroundImage: `url(${image})` }"
-            style="background-position: bottom right; background-size: 180px"
-            :disabled="loading"
-          >
+            style="background-position: bottom right; background-size: 180px" :disabled="loading">
             <img src="/icons/delete.svg" alt="Delete icon" />
           </button>
-          <button
-            type="submit"
+          <button type="submit"
             class="bg-deskree-600 hover:bg-deskree-300 transition-colors rounded-lg min-h-[39px] flex items-center justify-center mb-2 disabled:bg-deskree-300 disabled:opacity-40 disabled:cursor-not-allowed"
-            :disabled="loading"
-          >
+            :disabled="loading">
             <LoadingWidget v-if="loading" />
             <span v-else>Send</span>
           </button>
         </div>
       </form>
     </div>
-    <transition
-      enter-active-class="animate-fade-in-up"
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
-      leave-active-class="animate-fade-out-down"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-    >
-      <ToastNotification
-        v-show="showError"
-        @click="handleDismissError"
-        class="cursor-pointer inset-0 overflow-hidden transition-opacity"
-      >
+    <transition enter-active-class="animate-fade-in-up" enter-from-class="opacity-0" enter-to-class="opacity-100"
+      leave-active-class="animate-fade-out-down" leave-from-class="opacity-100" leave-to-class="opacity-0">
+      <ToastNotification v-show="showError" @click="handleDismissError"
+        class="cursor-pointer inset-0 overflow-hidden transition-opacity">
         {{ error }}
       </ToastNotification>
     </transition>
--- src/components/WidgetSuccess.vue
+++ src/components/WidgetSuccess.vue
@@ -15,6 +15,7 @@ const emit = defineEmits<{
 
 const feedbackSent = ref(false);
 const error = ref('');
+const showError = ref(false);
 
 const allStars: Ref<{ id: number; isHovered: boolean }[]> = ref([
   { id: 1, isHovered: ref(false) },
@@ -37,14 +38,7 @@ async function sendFeedback(rating: number) {
   };
   try {
     feedbackSent.value = true;
-    await fetch(
-      `https://${config.PROJECT_ID}.api.deskree.com/api/v1/rest/collections/feedbacks/${props.feedbackUid}`,
-      {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      }
-    );
+    // TODO: Set User's Feedback Rating
   } catch (e) {
     console.error(e);
   }
@@ -53,10 +47,8 @@ async function sendFeedback(rating: number) {
 function handleSendAnother() {
   emit('send-another');
   feedbackSent.value = false;
-  allStars.value.forEach(star => (star.isHovered = false));
+  allStars.value.forEach(star => star.isHovered = false);
 }
-
-const showError = ref(false);
 
 function handleDismissError() {
   showError.value = false;
@@ -73,8 +65,7 @@ watchEffect(() => {
 
 <template>
   <div
-    class="bg-[#1E1E20] rounded-2xl mb-4 shadow-lg w-[calc(100vw-2rem)] min-h-[330px] px-4 md:w-auto sm:w-auto flex flex-col justify-evenly"
-  >
+    class="bg-[#1E1E20] rounded-2xl mb-4 shadow-lg w-[calc(100vw-2rem)] min-h-[330px] px-4 md:w-auto sm:w-auto flex flex-col justify-evenly">
     <CloseButton />
     <div class="flex flex-col items-center mt-8 gap-4">
       <img src="/icons/circular-checkmark.svg" alt="" class="w-11.5 h-11.5" />
@@ -88,74 +79,33 @@ watchEffect(() => {
     <p class="text-xs opacity-60 font-normal mt-4 text-center">
       How would you rate your experience?
     </p>
-    <div
-      v-if="!feedbackSent"
-      class="flex items-center justify-center gap-3 pt-2"
-      :onMouseleave="() => handleAllStarsHover(-1)"
-    >
-      <div
-        v-for="star in allStars"
-        :key="star.id"
-        :onMouseover="() => handleAllStarsHover(star.id - 1)"
+    <div v-if="!feedbackSent" class="flex items-center justify-center gap-3 pt-2"
+      :onMouseleave="() => handleAllStarsHover(-1)">
+      <div v-for="star in allStars" :key="star.id" :onMouseover="() => handleAllStarsHover(star.id - 1)"
         @click="sendFeedback(star.id)"
-        class="hover:bg-blue-rgba hover:rounded-full w-6 h-6 flex items-center justify-center cursor-pointer"
-      >
-        <img
-          v-show="!star.isHovered"
-          src="/icons/star.svg"
-          alt="Star"
-          class="group-hover:opacity-0"
-        />
-        <img
-          v-show="star.isHovered"
-          src="/icons/filled-star.svg"
-          alt="Filled star"
-          class="group-hover:opacity-0"
-        />
+        class="hover:bg-blue-rgba hover:rounded-full w-6 h-6 flex items-center justify-center cursor-pointer">
+        <img v-show="!star.isHovered" src="/icons/star.svg" alt="Star" class="group-hover:opacity-0" />
+        <img v-show="star.isHovered" src="/icons/filled-star.svg" alt="Filled star" class="group-hover:opacity-0" />
       </div>
     </div>
     <div v-else class="flex items-center justify-center gap-3 pt-2">
-      <div
-        v-for="star in allStars"
-        :key="star.id"
-        class="hover:bg-transparent w-6 h-6 flex items-center justify-center"
-      >
-        <img
-          v-show="!star.isHovered"
-          src="/icons/empty-star.svg"
-          alt="Disabled star"
-          class="group-hover:opacity-0"
-        />
-        <img
-          v-show="star.isHovered"
-          src="/icons/filled-star.svg"
-          alt="Filled star"
-          class="group-hover:opacity-0"
-        />
+      <div v-for="star in allStars" :key="star.id"
+        class="hover:bg-transparent w-6 h-6 flex items-center justify-center">
+        <img v-show="!star.isHovered" src="/icons/empty-star.svg" alt="Disabled star" class="group-hover:opacity-0" />
+        <img v-show="star.isHovered" src="/icons/filled-star.svg" alt="Filled star" class="group-hover:opacity-0" />
       </div>
     </div>
     <div class="flex px-4 gap-2 w-full flex-col">
       <div class="flex flex-col gap-4 mt-2">
-        <span
-          @click="handleSendAnother"
-          class="text-xs text-deskree-600 hover:text-white text-center cursor-pointer mb-2 transition-colors"
-          >Send another feedback</span
-        >
+        <span @click="handleSendAnother"
+          class="text-xs text-deskree-600 hover:text-white text-center cursor-pointer mb-2 transition-colors">Send
+          another feedback</span>
       </div>
     </div>
-    <transition
-      enter-active-class="animate-fade-in-up"
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
-      leave-active-class="animate-fade-out-down"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-    >
-      <ToastNotification
-        v-show="showError"
-        @click="handleDismissError"
-        class="cursor-pointer inset-0 overflow-hidden transition-opacity"
-      >
+    <transition enter-active-class="animate-fade-in-up" enter-from-class="opacity-0" enter-to-class="opacity-100"
+      leave-active-class="animate-fade-out-down" leave-from-class="opacity-100" leave-to-class="opacity-0">
+      <ToastNotification v-show="showError" @click="handleDismissError"
+        class="cursor-pointer inset-0 overflow-hidden transition-opacity">
         {{ error }}
       </ToastNotification>
     </transition>
